f0ffbc26bdd37589c28bb6a4df0e778fa55b05af
Tu T. Do <tu.dothanh1906@gmail.com>
2022-06-30 16:28:59 +0700

    U: Done Homework 0

---
+++ hw0/main.js
@@ -0,0 +1,35 @@
+const Car = function(name, speed = 0) {
+    // Name : name of the car
+    // Speed: speed of the car
+    this.name = name;
+    this.speed = speed;
+
+    this.get_speed = function() {
+        return `${this.speed} m/s.`
+    }
+};
+
+Car.prototype.accelerate = function(t, a){
+    // t: number of sec
+    // a: acceleration rate (m/s^2)
+    console.log(`Accelerating at ${2} m/s^2 for ${t} seconds!!!`)
+    this.speed+=a*t;
+    return 0
+};
+
+Car.prototype.brake = function(){
+    console.log(`Braking ...`)
+    this.speed = 0;
+};
+
+
+
+
+bmw = new Car("BMW", 10);
+console.log(bmw.name, bmw.get_speed())
+
+bmw.accelerate(5, 2);
+console.log(bmw.name, bmw.get_speed())
+
+bmw.brake();
+console.log(bmw.name, bmw.get_speed())
